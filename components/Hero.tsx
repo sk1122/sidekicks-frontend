d1bbf4f69e760c54cb4c7c774471b42cea23bb21
Satyam Kulkarni <punekar.satyam@gmail.com>
2022-03-08 16:34:59 +0530

    Added OG Image & Added Sidekicks Logo

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,16 +1,19 @@
 const Hero = () => {
 	return (
-		<div className="w-full min-h-screen flex flex-col lg:flex-row justify-center items-center px-16 md:px-20 lg:px-40 bg-gradient space-y-10 md:space-y-0">
-			<div className="lg:hidden w-full md:w-1/2 h-full flex flex-col justify-center items-center">
-				<img src="/hero.png" alt="" />
-			</div>
-			<div className="w-full md:w-1/2 h-full flex flex-col justify-center items-start space-y-5">
-				<h1 className="w-20 text-[4em] md:text-[8em] xl:text-[10em] leading-none font-black -mb-5">BUILD, SHARE EARN</h1>
-				<p className="w-full lg:w-4/5 font-medium text-sm md:text-md">A New way of launching Projects and getting Incentivized for it. Create a project - Share with everyone - Project belivers will give you crypto kicks and share you the inputs </p>
-				<a href='https://tally.so/r/wdG5N3' target='_blank' className="p-4 rounded-xl bg-gradient-to-r from-[#FFD600] to-[#FF5C00] font-bold cursor-pointer hover:scale-110 duration-300">Get Early Access</a>
-			</div>
-			<div className="hidden lg:block w-full md:w-1/2 h-full flex flex-col justify-center items-center">
-				<img src="/hero.png" alt="" />
+		<div className="w-full min-h-screen flex flex-col justify-start pt-5 md:pt-10 lg:pt-20 items-center bg-gradient">
+			<img src="/sidekicks.png" className='w-fit h-fit' alt="" />
+			<div className="w-full h-full flex flex-col lg:flex-row justify-center items-center px-16 md:px-20 lg:px-40 space-y-10 md:space-y-0">
+				<div className="lg:hidden w-full md:w-1/2 h-full flex flex-col justify-center items-center">
+					<img src="/hero.png" alt="" />
+				</div>
+				<div className="w-full md:w-1/2 h-full flex flex-col justify-center items-start space-y-5">
+					<h1 className="w-20 text-[4em] md:text-[8em] xl:text-[10em] leading-none font-black -mb-5">BUILD, SHARE EARN</h1>
+					<p className="w-full lg:w-4/5 font-medium text-sm md:text-md">A New way of launching Projects and getting Incentivized for it. Create a project - Share with everyone - Project belivers will give you crypto kicks and share you the inputs </p>
+					<a href='https://tally.so/r/wdG5N3' target='_blank' className="p-4 rounded-xl bg-gradient-to-r from-[#FFD600] to-[#FF5C00] font-bold cursor-pointer hover:scale-110 duration-300">Get Early Access</a>
+				</div>
+				<div className="hidden lg:block w-full md:w-1/2 h-full flex flex-col justify-center items-center">
+					<img src="/hero.png" alt="" />
+				</div>
 			</div>
 		</div>
 	)
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,15 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='' />
         <link href="https://fonts.googleapis.com/css2?family=Urbanist:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+        
+        <link rel="shortcut icon" href="https://pbs.twimg.com/profile_images/1492844146465599488/lcpIKQSH_400x400.jpg" type="image/x-icon" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@sk1122_" />
+        <meta name="twitter:creator" content="@sk1122_" />
+        <meta property="og:url" content="https://side-kicks.xyz" />
+        <meta property="og:title" content="Sidekicks - Buidl, Share, Earn" />
+        <meta property="og:description" content="A New way of launching Projects and getting Incentivized for it. Create a project - Share with everyone - Project belivers will give you crypto kicks and share you the inputs" />
+        <meta property="og:image" content="/og.png" />
       </Head>
       <Component {...pageProps} />
     </div>
